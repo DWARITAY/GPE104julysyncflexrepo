024b157bacdbaf6234cbb8251f500c17610d0029
DWARITAY <derrickwaritay428@gmail.com>
2026-07-14 06:40:28 -0700

    Hello world script

---
+++ HelloWorld.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (false) 
+        {
+        Debug.Log("Expression is true");
+        }
+        else if (3 == 2) 
+        {
+        Debug.Log("Two is equal to two");
+        }
+        else 
+        {
+            Debug.Log("Two plus two equals five");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello, World!");
+    }
+}
